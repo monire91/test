2b9111feb1a40ff685b2c68f36bb90fb8d55a3a4
monire <aglida1370@gmail.com>
2022-06-14 22:59:01 +0430

    v

--- src/components/core/card/index.js
+++ src/components/core/card/index.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import img from '../../../assets/images/card-image-1.png'
 import calendar from '../../../assets/images/icons/calendar.svg'
 import {Link} from "react-router-dom";
+import API from "../../../api";
 
-const Card = ({movie}) => {
-    console.log("movie",movie)
+const Card = ({movie, genres}) => {
+
     return (
-        <div className='w-295 h-195 flex border-grey1 border-1 bg-grey2 rounded-md'>
-            {/*<Link to={`/post/${el.id}`}>*/}
-            <Link to={`/${movie.id}`}>
-            <img className='my-3 ml-1' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} style={{width:'125px'}}/>
-            <div className='flex flex-col pt-4 pl-11 pr-31 pb-13'>
-                <span className='font-bold h-44'>{movie.title}</span>
-                <div className='mt-64'>
-                    <div className='flex mb-3.5'>
-                        <img className='mr-7' src={calendar}/>
-                        <span className='text-xs text-black'>2014-10-22</span>
+        <Link to={`/${movie.id}`}>
+            <div className='w-295 h-195 flex border-grey1 border-1 bg-grey2 rounded-md mb-38' >
+                <img className='my-3 ml-1 w-128 rounded-bl-md rounded-tl-md'
+                     src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
+                <div className='flex flex-col pt-4 pl-11 pr-31 pb-13'>
+                    <span className='font-bold h-44 block-ellipsis'>{movie.title}</span>
+                    <div className='mt-42'>
+                        <div className='flex mb-3.5'>
+                            <img className='mr-7' src={calendar}/>
+                            <span className='text-xs text-black'>{movie.release_date}</span>
+                        </div>
+                        {genres.filter((item) => movie.genre_ids.includes(item.id)).map((item) => (
+                            <span className='text-xs' key={item.id}>{` ${item.name}, `}</span>))}
                     </div>
-                    ff
                 </div>
+
             </div>
-            </Link>
-        </div>
+        </Link>
     );
 };
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,28 +4,34 @@ import Card from "../../components/core/card";
 import API from "../../api";
 
 const Home = () => {
-    const [movies,setMovies]=useState('')
-    useEffect(() => {
-        API.get()
+    const [movies, setMovies] = useState('')
+    const [genres, setGenres] = useState('')
 
-        API.get(`discover/movie?api_key=65c777caa18dc18e557b9e42dbc46d62`)
-            .then(res => {
-                setMovies(res.data)
-            })
+    useEffect(() => {
+        const getMovies = API.get(`discover/movie?api_key=65c777caa18dc18e557b9e42dbc46d62`)
+        const getGenres = API.get(`/genre/movie/list?api_key=65c777caa18dc18e557b9e42dbc46d62`)
+
+        Promise.all([getGenres, getMovies]).then((values) => {
+            setGenres(values[0].data.genres)
+            setMovies(values[1].data)
+        });
     }, [])
-    // console.log("movies",movies.results)
+
     return (
         <div>
-           <div className='bg-grey rounded-md pt-27 pb-25 pl-81 pr-62 flex items-center mb-119' >
-               <span className='mr-29'>Search by release date:</span>
-               <input className='w-221 h-33 border-grey1 border-1 p-3'/>
-               <div className='ml-auto'>
-                   <Button title='Search'/>
-               </div>
-           </div>
-            {movies?.results?.map(item=>(
-                <Card movie={item}/>
-            ))}
+            <div className='bg-grey rounded-md pt-27 pb-25 pl-81 pr-62 flex items-center mb-119'>
+                <span className='mr-29'>Search by release date:</span>
+                <input className='w-221 h-33 border-grey1 border-1 p-3'/>
+                <div className='ml-auto'>
+                    <Button title='Search'/>
+                </div>
+            </div>
+            {/*<div className='flex flex-wrap justify-between after:content-[""] after:flex-auto'>*/}
+            <div className='flex flex-wrap movies'>
+                {movies?.results?.map(item => (
+                    <Card key={item.id} movie={item} genres={genres}/>
+                ))}
+            </div>
         </div>
     );
 };
